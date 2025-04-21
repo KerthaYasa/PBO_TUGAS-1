f1c086d73ad20e89d505e8b8ab851103acde30a5
kerthayasa <yasa.24034@student.unud.ac.id>
2025-04-21 17:47:52 +0700

    Penambahan Class SuratBerhargaNegara

---
+++ Source/SuratBerhargaNegara.java
@@ -0,0 +1,41 @@
+package model;
+
+public class SuratBerhargaNegara {
+    private String nama;
+    private double bunga;
+    private int jangkaWaktu;
+    private String tanggalJatuhTempo;
+    private double kuotaNasional;
+
+    public SuratBerhargaNegara(String nama, double bunga, int jangkaWaktu, String tanggalJatuhTempo, double kuotaNasional) {
+        this.nama = nama;
+        this.bunga = bunga;
+        this.jangkaWaktu = jangkaWaktu;
+        this.tanggalJatuhTempo = tanggalJatuhTempo;
+        this.kuotaNasional = kuotaNasional;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public double getBunga() {
+        return bunga;
+    }
+
+    public int getJangkaWaktu() {
+        return jangkaWaktu;
+    }
+
+    public String getTanggalJatuhTempo() {
+        return tanggalJatuhTempo;
+    }
+
+    public double getKuotaNasional() {
+        return kuotaNasional;
+    }
+
+    public void kurangiKuota(double nominal) {
+        this.kuotaNasional -= nominal;
+    }
+}
