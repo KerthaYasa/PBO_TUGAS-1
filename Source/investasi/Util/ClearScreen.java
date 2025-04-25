4186ba2e6106c9cdefbc629eb2644a6f45e963b7
daffa <joojahyun@gmail.com>
2025-04-25 21:56:24 +0800

    Pembuatan classs clearscreen

---
+++ Source/investasi/Util/ClearScreen.java
@@ -0,0 +1,16 @@
+package investasi.Util;
+
+public class ClearScreen {
+    public static void clear() {
+        try {
+            if (System.getProperty("os.name").contains("Windows")) {
+                new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+            } else {
+                System.out.print("\033[H\033[2J");
+                System.out.flush();
+            }
+        } catch (Exception e) {
+            System.out.println("Gagal membersihkan layar: " + e.getMessage());
+        }
+    }
+}
